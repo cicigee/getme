8b0bac8b56f6474ce18427d8bd735b15f4fb3a64
Luke Gripenberg <lukegee@users.noreply.github.com>
2017-10-02 11:04:01 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$path="/tmp/";
+$fileindexfile=$path."index";
+$config['sites']['example.com']=Array('username'=>'myuser','password'=>'mypassword');
+
+$urlpath=$path."urls/";
+$logpath=$path."logs/";
+$mappingpath=$path."mapping/";
+$incompletepath=$path."incomplete/";
+$archivepath=$path."archive/";
+?>
